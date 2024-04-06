ec3992d447254472c1a33731b678b04e3383d4af
Allen John <allenjohnmonapallil@gmail.com>
2024-04-06 21:13:33 +0530

    changes changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {Auth} from './pages/auth/index.jsx'
 import {ExpenseTracker} from './pages/expense-tracker/index.jsx'
 
+
 function App() {
   return (
     <div>
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,8 +3,6 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 
-
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
